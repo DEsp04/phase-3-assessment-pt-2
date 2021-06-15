5941e31bcf6fb50ac4b4e8f8afced5845331c739
David Espinal <despinal0425@gmail.com>
2021-06-15 19:57:01 -0400

    connection to mongodb was made

---
+++ db/index.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+// require("dotenv").config();
+
+
+// let MONGODB_URI = process.env.PROD_MONGODB || process.env.MONGODB_URI || process.env.DB_Cluster_URL;
+
+// console.log(MONGODB_URI)
+
+let MONGODB_URI =
+  process.env.PROD_MONGODB ||
+  process.env.MONGODB_URI ||
+  "mongodb://127.0.0.1:27017/airlineDatabase";
+
+
+mongoose.connect(MONGODB_URI, {
+  useUnifiedTopology: true, useNewUrlParser:true
+}).then(() => {
+  console.log("Successfully connected to MongoDB")
+}).catch(e => {
+  console.error("Connection error", e.message)
+})
+
+
+const db = mongoose.connection;
+module.exports = db;
