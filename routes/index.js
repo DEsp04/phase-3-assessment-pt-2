99f005b14df838a6337928119131fc7ad9b86d90
David Espinal <despinal0425@gmail.com>
2021-06-15 21:01:28 -0400

    completed the router

---
+++ routes/index.js
@@ -0,0 +1,37 @@
+const { Router } = require("express");
+const router = Router();
+const controllers = require("../controllers");
+
+
+router.get("/", (req, res) => res.send("This is root!"));
+
+
+router.post("/lambDishes", controllers.createLambDish);
+
+router.get("/lambDishes", controllers.getAllLambDish);
+
+router.get("/lambDishes/:id", controllers.getLambDishById);
+
+router.post("/lambDishes/:id", controllers.updateLambDish);
+
+router.delete("/lambDishes/:id", controllers.deleteLambDish);
+
+
+router.get("/homeCountries/:id/lambDishes", controllers.getLambDishByCountry)
+
+
+router.post("/homeCountries", controllers.createHomeCountry);
+
+router.get("/homeCountries", controllers.getAllHomeCountry);
+
+router.get("/homeCountries/:id", controllers.getHomeCountryById);
+
+router.post("/homeCountries/:id", controllers.updateHomeCountry);
+
+router.delete("/homeCountries/:id", controllers.deleteHomeCountry);
+
+module.exports = router;
+
+
+
+
